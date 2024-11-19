ff02d3fb0baeb58bc75d5b7cc05c671b6248e6ae
Ahmad Zaqiq <160167174+AhmadZaqiq@users.noreply.github.com>
2024-11-19 20:27:18 +0200

    Update and rename Problem 05.sql to Problem 05 Get All Makes that have manufactured more than 12000 Vehicles in years 1950 to 2000.sql

--- Queries/Problem 05 Get All Makes that have manufactured more than 12000 Vehicles in years 1950 to 2000.sql
+++ Queries/Problem 05 Get All Makes that have manufactured more than 12000 Vehicles in years 1950 to 2000.sql
@@ -1,8 +1,9 @@
+Select * From
+(
 Select Makes.Make, Count(*) AS NumberOfVehicles 
 From VehicleDetails
 Join Makes
 On Makes.MakeID=VehicleDetails.MakeID
 Where Year Between 1950 and 2000
 Group By(Make)
-Having Count(*)>12000
-Order By NumberOfVehicles Desc
+) R1 Where R1.NumberOfVehicles>12000
